c8a49dcb8df60d73e78b3c9f6a1b0d98a81a1aa2
Muhammad Farhan <gfarhan18@gmail.com>
2023-09-05 15:22:45 +0500

    worked on final touches

--- views/display.php
+++ views/display.php
@@ -6,20 +6,14 @@
 </head>
 <body>
     <?php
-    // Include the DisplayController class
     require_once '../controllers/DisplayController.php';
-    require_once '../db.php'; // Adjust the path as needed
-
-    // Get username and token from query parameters
+    require_once '../db.php'; 
     $username = $_GET['username'];
     $token = $_GET['token'];
 
-    // Include the existing database connection
 
-    // Create an instance of the DisplayController and pass the database connection
     $displayController = new DisplayController($db);
 
-    // Call the displayUserDetails method to show user details
     $userDetails = $displayController->displayUserDetails($username, $token);
     ?>
     <div class="user-details">
@@ -29,7 +23,6 @@
             echo "<table>";
             echo "<tr><td>Name:</td><td>" . $userDetails['username'] . "</td></tr>";
             echo "<tr><td>Token:</td><td>" . $userDetails['token'] . "</td></tr>";
-            // Add more details as needed
             echo "</table>";
         } else {
             echo "<p class='error-message'>Token expired or invalid.</p>";
